d718b4a4b86f01a3c64e9c7e42c79d76683892a7
wzzzd <609935229@qq.com>
2021-12-28 17:59:29 +0800

    修改代码架构

--- run.py
+++ run.py
@@ -9,9 +9,6 @@ import torch
 import logging
 from Config import Config
 from DataManager import DataManager
-from process.train import train
-from process.eval import eval
-from process.test import test
 from process.Trainer import Trainer
 from process.Predictor import Predictor
 
@@ -32,31 +29,6 @@ if __name__ == '__main__':
     # 数据处理
     print('read data...')
     dm = DataManager()
-    # 获取参数
-    # num_labels = dm.num_labels
-    # tgt2index = dm.tgt2index
-    # index2tgt = dm.index2tgt
-
-    # # 模式
-    # if Config.mode == 'train':
-    #     # 获取数据
-    #     print('data process...')
-    #     train_loader = dm.get_data_train()
-    #     valid_loader = dm.get_data_valid()
-    #     # 训练
-    #     train(train_loader, valid_loader, num_labels, index2tgt)
-    # elif Config.mode == 'eval':
-    #     # 评估
-    #     valid_loader = dm.get_data_valid()
-    #     # eval(valid_loader, model, id2label, func_index2token)
-    #     pass
-    # elif Config.mode == 'test':
-    #     # 测试
-    #     test_loader = dm.get_data_test()
-    #     test(Config, test_loader, index2tgt)
-    # else:
-    #     print("no task going on!")
-    #     print("you can use one of the following lists to replace the valible of Config.py. ['train', 'test', 'valid'] !")
     
     # 模式
     if config.mode == 'train':
